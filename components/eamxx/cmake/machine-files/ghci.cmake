c4ca4407da4e3e2c06a3976e9dcfe0bd883c9633
Luca Bertagna <lbertag@sandia.gov>
2024-09-17 18:10:21 -0600

    EAMxx: Fix ghci mach file
    Allow test-launcher to manage resources

--- components/eamxx/cmake/machine-files/ghci.cmake
+++ components/eamxx/cmake/machine-files/ghci.cmake
@@ -10,3 +10,6 @@ set(LAPACK_LIBRARIES "/spack-installs/netlib-lapack/3.11.0/gcc/12.3.0/base/65x6u
 
 # Let's catch usage of code deprecated in Kokkos 4
 option (Kokkos_ENABLE_DEPRECATED_CODE_4 "" OFF)
+
+# We need to manage resources to spread across available cores/gpus
+option (EKAT_TEST_LAUNCHER_MANAGE_RESOURCES "" ON)
